4460403547af43756987a94b90658875ca0bfe13
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-07-30 22:18:43 +0900

    add TitleTableViewCell

---
+++ CalendarApp/TitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TitleTableViewCell.swift
+//  CalendarApp
+//
+//  Created by 櫻本静香 on 2016/07/30.
+//  Copyright © 2016年 Sakuramoto Shizuka. All rights reserved.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
