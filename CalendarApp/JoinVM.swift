10a92428ceb8da870aa58a981cd2f07b95223efa
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-08-10 02:38:10 +0900

    Can view to join or reject button and the calendars invited information

---
+++ CalendarApp/JoinVM.swift
@@ -0,0 +1,40 @@
+//
+//  JoinVM.swift
+//  CalendarApp
+//
+//  Created by 櫻本静香 on 2016/08/07.
+//  Copyright © 2016年 Sakuramoto Shizuka. All rights reserved.
+//
+
+import UIKit
+
+class JoinVM: NSObject, UICollectionViewDataSource {
+    
+    var users = [User]()
+    
+    // MARK - CollectionViewDataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! SelectedUserCollectionViewCell
+        let user = users[indexPath.row]
+        cell.fillWith(user)
+        return cell
+    }
+    
+    // MARK -> Collectionview header
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let headerReusableView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.nibName, forIndexPath: indexPath) as! HeaderCollectionReusableView
+            
+            return headerReusableView
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+
+}
