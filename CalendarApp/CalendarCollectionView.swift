97df6ddd497ec8a094ce6d08df62fe1e88a30f58
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-08-25 10:33:23 +0900

    change png

---
+++ CalendarApp/CalendarCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarCollectionView.swift
+//  CalendarApp
+//
+//  Created by 櫻本静香 on 2016/08/24.
+//  Copyright © 2016年 Sakuramoto Shizuka. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCollectionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
